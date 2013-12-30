0e2159ea42a621ac75c4558cae69dea00a7e2c8f
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-12-30 14:18:20 +0800

    change show a bit for attendance. amazon ses

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: ENV['AMAZON_SES_ACCESS_KEY'],#
-  secret_access_key: ENV['AMAZON_SES_SECRET_KEY']#
+  access_key_id: ENV['AMAZON_ACCESS_KEY'],#
+  secret_access_key: ENV['AMAZON_SECRET_KEY']#
