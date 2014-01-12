24674f7784c321e4132ca78d5480f9f5ada006a2
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-01-12 18:38:05 +0800

    events helper fix. for when image url has params after.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,13 @@
 module EventsHelper
   def display_uploaded_file(file)
-    extension = file.to_s.split('.').last.try(:downcase)
+    extension = file.to_s.split('.').last.try(:downcase).try(:split,'?').try(:first)
     if extension == 'pdf'
       
       content_tag(:a, href: file, class: 'image-link') do image_tag('pdf-icon.png') end
     elsif extension.in?(['png','jpg','gif','jpeg'])
       content_tag(:a, href: file, class: 'image-link') do image_tag(file.thumb) end
     else
-        'Tiada Fail Dijumpai' + extension
+        'Tiada Fail Dijumpai'
     end
   end
 
