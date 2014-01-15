b8a113b6f5280b3ca99b5f5a5331dcca8194e892
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-01-15 20:17:41 +0800

    welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  layout 'empty', only: [:welcome]
   protect_from_forgery
   def city_dropdown
     @cities = City.where(state_name: params[:state])
     respond_to do |format|
       format.html { render layout: false }
     end
+  end
+
+
+  def welcome
+
   end
 end
