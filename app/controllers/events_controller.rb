c8ef069652e24e453d809e00d8d604a18640f2b3
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2014-05-24 00:58:13 +0800

    update rsvp also don't need sign in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
 
-  skip_before_filter :authenticate_user!, only: [ :open_email, :terima_kasih, :coming, :track_open]
+  skip_before_filter :authenticate_user!, only: [ :open_email, :terima_kasih, :coming, :track_open, :update_rsvp]
   layout 'empty', only: [:terima_kasih, :coming]
   def index
     @events = Event.order('date desc')
